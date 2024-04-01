6847bfe342cf82d91b98b854cf21d3ba93eaabf8
keddo <adu.kedir.abdurahman@gmail.com>
2024-04-01 14:08:58 +0300

    Added hell, greet/<name>, and add/<n1>/<n2>

--- app.py
+++ app.py
@@ -6,6 +6,19 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello, World!</h1>"
 
+@app.route('/hello')
+def hello():
+    return "Hell, world!"
+
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f"Hello,{name}"
+
+@app.route('/add/<number1>/<number2>')
+def add(number1, number2):
+    return f"{number1} + {number2} = {number1 + number2}"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3500, debug=True)
