8b1aba49238abe7157f26d456bd12deb8effa633
keddo <adu.kedir.abdurahman@gmail.com>
2024-04-01 14:47:05 +0300

    Custom response

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
 
@@ -8,12 +8,16 @@ def index():
 
 @app.route('/hello', methods=["POST", "GET"])
 def hello():
-    if request.method == 'GET':
-        return "You made, a GET request", 200
-    elif request.method == "POST":
-        return "You made a POST request", 201
-    else:
-        return "You will never see this message."
+    # if request.method == 'GET':
+    #     return "You made, a GET request", 200
+    # elif request.method == "POST":
+    #     return "You made a POST request", 201
+    # else:
+    #     return "You will never see this message."
+    res = make_response()
+    res.status_code = 201
+    res.headers['content-type'] = 'applicaion/octet-stream'
+    return res
 
 
 @app.route('/greet/<name>')
